b78f26269ba8f1f0f4897bccf97f3f16792b9ecc
fhmanwar <fhmianwar@gmail.com>
2020-07-27 09:58:36 +0700

    update modul LineChart

--- Client/wwwroot/js/Scripts/HomeScript.js
+++ Client/wwwroot/js/Scripts/HomeScript.js
@@ -38,7 +38,7 @@ var Konsumenchart = am4core.createFromConfig({
         "children": [{
             "type": "Label",
             "forceCreate": true,
-            "text": "Konsumen",
+            "text": "Car",
             "horizontalCenter": "middle",
             "verticalCenter": "middle",
             "fontSize": 40
@@ -56,135 +56,240 @@ function exportPDF() {
     Konsumenchart.exporting.export("pdf");
 }
 
-// =================================================================
 // Create chart instance
 var chart = am4core.create("carChart", am4charts.XYChart);
 
 // Increase contrast by taking evey second color
 chart.colors.step = 2;
-
 // Add data
-//chart.data = generateChartData();
-//chart.data = getData();
-
+chart.data = getDataLine();
+// Set input format for the dates
+chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
 // Create axes
 var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-dateAxis.renderer.minGridDistance = 50;
-
+/* dateAxis.renderer.grid.template.location = 0; */
+/* dateAxis.renderer.minGridDistance = 50; */
+dateAxis.renderer.grid.template.disabled = true;
+/* dateAxis.renderer.fullWidthTooltip = true; */
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 // Create series
-function createAxisAndSeries(field, name, opposite, bullet) {
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    if (chart.yAxes.indexOf(valueAxis) != 0) {
-        valueAxis.syncWithAxis = chart.yAxes.getIndex(0);
-    }
+var series = chart.series.push(new am4charts.LineSeries());
+series.dataFields.valueY = "durasi";
+series.dataFields.dateX = "date";
+series.tooltipText = "{value}"
+series.strokeWidth = 2;
+series.minBulletDistance = 15;
 
-    var series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.valueY = field;
-    series.dataFields.dateX = "date";
-    series.strokeWidth = 2;
-    series.yAxis = valueAxis;
-    series.name = name;
-    series.tooltipText = "{name}: [bold]{valueY}[/]";
-    series.tensionX = 0.8;
-    series.showOnInit = true;
+// Drop-shaped tooltips
+series.tooltip.background.cornerRadius = 20;
+series.tooltip.background.strokeOpacity = 0;
+series.tooltip.pointerOrientation = "vertical";
+series.tooltip.label.minWidth = 40;
+series.tooltip.label.minHeight = 40;
+series.tooltip.label.textAlign = "middle";
+series.tooltip.label.textValign = "middle";
 
-    var interfaceColors = new am4core.InterfaceColorSet();
+// Make bullets grow on hover
+var bullet = series.bullets.push(new am4charts.CircleBullet());
+bullet.circle.strokeWidth = 2;
+bullet.circle.radius = 4;
+bullet.circle.fill = am4core.color("#fff");
 
-    switch (bullet) {
-        case "triangle":
-            var bullet = series.bullets.push(new am4charts.Bullet());
-            bullet.width = 12;
-            bullet.height = 12;
-            bullet.horizontalCenter = "middle";
-            bullet.verticalCenter = "middle";
+var bullethover = bullet.states.create("hover");
+bullethover.properties.scale = 1.3;
 
-            var triangle = bullet.createChild(am4core.Triangle);
-            triangle.stroke = interfaceColors.getFor("background");
-            triangle.strokeWidth = 2;
-            triangle.direction = "top";
-            triangle.width = 12;
-            triangle.height = 12;
-            break;
-        case "rectangle":
-            var bullet = series.bullets.push(new am4charts.Bullet());
-            bullet.width = 10;
-            bullet.height = 10;
-            bullet.horizontalCenter = "middle";
-            bullet.verticalCenter = "middle";
-
-            var rectangle = bullet.createChild(am4core.Rectangle);
-            rectangle.stroke = interfaceColors.getFor("background");
-            rectangle.strokeWidth = 2;
-            rectangle.width = 10;
-            rectangle.height = 10;
-            break;
-        default:
-            var bullet = series.bullets.push(new am4charts.CircleBullet());
-            bullet.circle.stroke = interfaceColors.getFor("background");
-            bullet.circle.strokeWidth = 2;
-            break;
-    }
-
-    valueAxis.renderer.line.strokeOpacity = 1;
-    valueAxis.renderer.line.strokeWidth = 2;
-    valueAxis.renderer.line.stroke = series.stroke;
-    valueAxis.renderer.labels.template.fill = series.stroke;
-    valueAxis.renderer.opposite = opposite;
-
-    // Create a horizontal scrollbar with previe and place it underneath the date axis
-    chart.scrollbarX = new am4charts.XYChartScrollbar();
-    chart.scrollbarX.series.push(series);
-    chart.scrollbarX.parent = chart.bottomAxesContainer;
-
-    dateAxis.start = 0.79;
-    dateAxis.keepSelection = true;
-}
-
-// Add legend
-chart.legend = new am4charts.Legend();
-
-//createAxisAndSeries("total", "Visits", false, "circle");
-createAxisAndSeries("visits", "Visits", false, "circle");
-//createAxisAndSeries("views", "Views", true, "triangle");
-//createAxisAndSeries("hits", "Hits", true, "rectangle");
-
-// Add cursor
+// Make a panning cursor
 chart.cursor = new am4charts.XYCursor();
+chart.cursor.behavior = "panXY";
+chart.cursor.xAxis = dateAxis;
+chart.cursor.snapToSeries = series;
+chart.cursor.fullWidthLineX = true;
+chart.cursor.lineX.strokeOpacity = 0;
+chart.cursor.lineX.fill = am4core.color("#000");
+chart.cursor.lineX.fillOpacity = 0.1;
 
-// generate some random data, quite different range
-function generateChartData() {
-    var chartData = [];
-    var firstDate = new Date();
-    firstDate.setDate(firstDate.getDate() - 100);
-    firstDate.setHours(0, 0, 0, 0);
+// Create a horizontal scrollbar with previe and place it underneath the date axis
+chart.scrollbarX = new am4charts.XYChartScrollbar();
+chart.scrollbarX.series.push(series);
+chart.scrollbarX.parent = chart.bottomAxesContainer;
 
-    var visits = 1600;
-    var hits = 2900;
-    var views = 8700;
+dateAxis.start = 0.79;
+dateAxis.keepSelection = true;
 
-    for (var i = 0; i < 15; i++) {
-        // we create date objects here. In your data, you can have date strings
-        // and then set format of your dates using chart.dataDateFormat property,
-        // however when possible, use date objects, as this will speed up chart rendering.
-        var newDate = new Date(firstDate);
-        newDate.setDate(newDate.getDate() + i);
+function getDataLine() {
+    var arrData = [];
+    $.ajax({
+        type: 'GET',
+        url: "/reserves/LoadReserve/",
+        success: function (data) {
+            console.log(data);
+            var group = data.reduce((res, value) => {
+                res[value.carName] = (res[value.carName] || 0) + 1;
+                return res;
+            }, {});
+            //for (var i = 0; i < Object.keys(group).length; i++) {
+            //    arrData.push({
+            //        car: Object.keys(group)[i],
+            //        total: Object.values(group)[i],
+            //    });
+            //}
+            $.each(data, function (i, val) {
+                var getStart = new Date(val.start_date);
+                var getEnd = new Date(val.end_date);
+                //console.log(getStart);
+                var diff = new Date(getEnd - getStart);
+                var days = diff / 1000 / 60 / 60 / 24;
+                console.log(days);
 
-        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-        hits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-        views += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-
-        chartData.push({
-            date: newDate,
-            visits: visits,
-            hits: hits,
-            views: views
-        });
-    }
-    console.log(chartData);
-    return chartData;
+                //arrData.push({
+                //    date: getStart,
+                //    durasi: days,
+                //    car: Object.keys(group)[i],
+                //    total: Object.values(group)[i],
+                //});
+                var dateStart = getStart.getFullYear() + '-' + ("0" + (getStart.getMonth() + 1)).slice(-2) + '-' + ("0" + getStart.getDate()).slice(-2);
+                arrData.push({
+                    date: dateStart,
+                    durasi: days,
+                    car: val.carName,
+                });
+            });
+            console.log(arrData);
+            return arrData;
+        }
+    });
 }
+getDataLine();
 
+// =================================================================
+//// Create chart instance
+//var chart = am4core.create("carChart", am4charts.XYChart);
 
+//// Increase contrast by taking evey second color
+//chart.colors.step = 2;
+
+//// Add data
+//chart.data = generateChartData();
+////chart.data = getDataLine();
+
+//// Create axes
+//var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+//dateAxis.renderer.minGridDistance = 50;
+
+//// Create series
+//function createAxisAndSeries(field, name, opposite, bullet) {
+//    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+//    if (chart.yAxes.indexOf(valueAxis) != 0) {
+//        valueAxis.syncWithAxis = chart.yAxes.getIndex(0);
+//        //console.log(chart.yAxes)
+//    }
+
+//    var series = chart.series.push(new am4charts.LineSeries());
+//    series.dataFields.valueY = field;
+//    series.dataFields.dateX = "date";
+//    series.strokeWidth = 2;
+//    series.yAxis = valueAxis;
+//    series.name = name;
+//    series.tooltipText = "{name}: [bold]{valueY}[/]";
+//    series.tensionX = 0.8;
+//    series.showOnInit = true;
+
+//    var interfaceColors = new am4core.InterfaceColorSet();
+
+//    switch (bullet) {
+//        case "triangle":
+//            var bullet = series.bullets.push(new am4charts.Bullet());
+//            bullet.width = 12;
+//            bullet.height = 12;
+//            bullet.horizontalCenter = "middle";
+//            bullet.verticalCenter = "middle";
+
+//            var triangle = bullet.createChild(am4core.Triangle);
+//            triangle.stroke = interfaceColors.getFor("background");
+//            triangle.strokeWidth = 2;
+//            triangle.direction = "top";
+//            triangle.width = 12;
+//            triangle.height = 12;
+//            break;
+//        case "rectangle":
+//            var bullet = series.bullets.push(new am4charts.Bullet());
+//            bullet.width = 10;
+//            bullet.height = 10;
+//            bullet.horizontalCenter = "middle";
+//            bullet.verticalCenter = "middle";
+
+//            var rectangle = bullet.createChild(am4core.Rectangle);
+//            rectangle.stroke = interfaceColors.getFor("background");
+//            rectangle.strokeWidth = 2;
+//            rectangle.width = 10;
+//            rectangle.height = 10;
+//            break;
+//        default:
+//            var bullet = series.bullets.push(new am4charts.CircleBullet());
+//            bullet.circle.stroke = interfaceColors.getFor("background");
+//            bullet.circle.strokeWidth = 2;
+//            break;
+//    }
+
+//    valueAxis.renderer.line.strokeOpacity = 1;
+//    valueAxis.renderer.line.strokeWidth = 2;
+//    valueAxis.renderer.line.stroke = series.stroke;
+//    valueAxis.renderer.labels.template.fill = series.stroke;
+//    valueAxis.renderer.opposite = opposite;
+
+//    // Create a horizontal scrollbar with previe and place it underneath the date axis
+//    chart.scrollbarX = new am4charts.XYChartScrollbar();
+//    chart.scrollbarX.series.push(series);
+//    chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+//    dateAxis.start = 0.79;
+//    dateAxis.keepSelection = true;
+//}
+
+//// Add legend
+//chart.legend = new am4charts.Legend();
+
+//createAxisAndSeries("days", "Durasi", false, "circle");
+//createAxisAndSeries("visits", "Visits", false, "circle");
+////createAxisAndSeries("views", "Views", true, "triangle");
+////createAxisAndSeries("hits", "Hits", true, "rectangle");
+
+//// Add cursor
+//chart.cursor = new am4charts.XYCursor();
+
+//// generate some random data, quite different range
+//function generateChartData() {
+//    var chartData = [];
+//    var firstDate = new Date();
+//    firstDate.setDate(firstDate.getDate() - 100);
+//    firstDate.setHours(0, 0, 0, 0);
+
+//    var visits = 1600;
+//    var hits = 2900;
+//    var views = 8700;
+
+//    for (var i = 0; i < 15; i++) {
+//        // we create date objects here. In your data, you can have date strings
+//        // and then set format of your dates using chart.dataDateFormat property,
+//        // however when possible, use date objects, as this will speed up chart rendering.
+//        var newDate = new Date(firstDate);
+//        newDate.setDate(newDate.getDate() + i);
+
+//        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+//        hits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+//        views += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+
+//        chartData.push({
+//            date: newDate,
+//            visits: visits,
+//            hits: hits,
+//            views: views
+//        });
+//    }
+//    //console.log(chartData);
+//    return chartData;
+//}
+// =================================================================
 
 function getData() {
     $.ajax({
